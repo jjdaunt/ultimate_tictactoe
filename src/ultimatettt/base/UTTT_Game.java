c317dd4decbcc1d4c22e24bd9e7c567b36bf4aba
Jim Daunt <jjdaunt@gmail.com>
2017-01-03 17:51:09 -0500

    medium AI

--- src/ultimatettt/base/UTTT_Game.java
+++ src/ultimatettt/base/UTTT_Game.java
@@ -122,8 +122,9 @@ public class UTTT_Game extends JFrame {
 		}
 	}
 	
-	// The AI lives here.
-	public void AImove(){
+	/// BEGIN AI ///
+	
+	private void AImove(){
 		if (difficulty == 1) moveEasy();
 		else if (difficulty == 2) moveMedium();
 		else moveHard();
@@ -131,7 +132,7 @@ public class UTTT_Game extends JFrame {
 		changePlayer();
 	}
 	
-	public void moveEasy(){
+	private void moveEasy(){
 		// Difficulty 1, literally moves randomly until success. Not great.
 		for (;;) {
 			int i = randInt(0, 2);
@@ -149,16 +150,174 @@ public class UTTT_Game extends JFrame {
 		}
 	}
 	
-	public void moveMedium(){
-		// Difficulty 2, actually tries. Will win boards (and games) if it can, but doesn't think beyond that.
-		
+	private void moveMedium(){
+		// Difficulty 2, actually tries. Priority: win game, prevent loss, win board, block board, fallback to moveEasy
+		move: for (;;) {
+			// if there is a board that can be played on and won, do that
+			for (int i = 0; i < 9; i++) {
+				if (boards[i/3][i%3].playable && gameWinnable(i, 2)){
+					int target = boardWinnable(i, 2);
+					if (target > -1){
+						if (this.boards[i/3][i%3].play(player, target/3, target%3)) {
+							if (getPlayer() > 0) setPlayables(target/3, target%3);
+							break move;
+						}
+					}
+				}
+			}
+			// block opponent's win
+			for (int i = 0; i < 9; i++) {
+				if (boards[i/3][i%3].playable && gameWinnable(i, 1)){
+					int target = boardWinnable(i, 1);
+					if (target > -1){
+						if (this.boards[i/3][i%3].play(player, target/3, target%3)) {
+							if (getPlayer() > 0) setPlayables(target/3, target%3);
+							break move;
+						}
+					}
+				}
+			}
+			// simply try to win a board
+			for (int i = 0; i < 9; i++){
+				int target = boardWinnable(i, 2);
+				if (target > -1){
+					if (this.boards[i/3][i%3].play(player, target/3, target%3)) {
+						if (getPlayer() > 0) setPlayables(target/3, target%3);
+						break move;
+					}
+				}
+			}
+			// try to block a won board for opponent
+			for (int i = 0; i < 9; i++){
+				int target = boardWinnable(i, 1);
+				if (target > -1){
+					if (this.boards[i/3][i%3].play(player, target/3, target%3)) {
+						if (getPlayer() > 0) setPlayables(target/3, target%3);
+						break move;
+					}
+				}
+			}
+			// failing any of these, play randomly.
+			moveEasy();
+			break move;
+		}
 	}
 	
-	public void moveHard(){
+	private void moveHard(){
 		// Difficulty 3, has some semblance of a plan to win the game.
 		
 	}
 	
+	/// BEGIN AI HELPERS ///
+	
+	// takes board from 0 to 8 to see if winning it wins the game
+	private boolean gameWinnable(int board, int player){
+		switch (board) {
+			case 0:
+				if (boards[0][1].getOwner() == player && boards[0][2].getOwner() == player) return true;
+				if (boards[1][0].getOwner() == player && boards[2][0].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				break;
+			case 1:
+				if (boards[0][0].getOwner() == player && boards[0][2].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[1][2].getOwner() == player) return true;
+				break;
+			case 2:
+				if (boards[0][1].getOwner() == player && boards[0][0].getOwner() == player) return true;
+				if (boards[1][2].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[2][0].getOwner() == player) return true;
+				break;
+			case 3:
+				if (boards[0][0].getOwner() == player && boards[2][0].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[1][2].getOwner() == player) return true;
+				break;
+			case 4:
+				if (boards[0][0].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				if (boards[2][0].getOwner() == player && boards[0][2].getOwner() == player) return true;
+				if (boards[0][1].getOwner() == player && boards[2][1].getOwner() == player) return true;
+				if (boards[1][0].getOwner() == player && boards[1][2].getOwner() == player) return true;
+				break;
+			case 5:
+				if (boards[0][2].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[1][0].getOwner() == player) return true;
+				break;
+			case 6:
+				if (boards[0][0].getOwner() == player && boards[1][0].getOwner() == player) return true;
+				if (boards[2][1].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				if (boards[1][1].getOwner() == player && boards[0][2].getOwner() == player) return true;
+				break;
+			case 7:
+				if (boards[0][1].getOwner() == player && boards[1][1].getOwner() == player) return true;
+				if (boards[2][0].getOwner() == player && boards[2][2].getOwner() == player) return true;
+				break;
+			case 8:
+				if (boards[1][1].getOwner() == player && boards[0][0].getOwner() == player) return true;
+				if (boards[1][2].getOwner() == player && boards[0][2].getOwner() == player) return true;
+				if (boards[2][1].getOwner() == player && boards[2][0].getOwner() == player) return true;
+				break;
+			default:
+				break;
+		}
+		return false;
+	}
+
+	// AI determining if a board is winnable. return -1 for false, 0-8 for target square
+	private int boardWinnable(int board, int player){
+		Board b = boards[board/3][board%3];
+		if (!b.playable) return -1;
+		int target = -1;
+		for (int i = 0; i < 9; i++){
+			switch (i) {
+				case 0:
+					if (b.squares[0][1].getOwner() == player && b.squares[0][2].getOwner() == player) target = i;
+					if (b.squares[1][0].getOwner() == player && b.squares[2][0].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					break;
+				case 1:
+					if (b.squares[0][0].getOwner() == player && b.squares[0][2].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[1][2].getOwner() == player) target = i;
+					break;
+				case 2:
+					if (b.squares[0][1].getOwner() == player && b.squares[0][0].getOwner() == player) target = i;
+					if (b.squares[1][2].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[2][0].getOwner() == player) target = i;
+					break;
+				case 3:
+					if (b.squares[0][0].getOwner() == player && b.squares[2][0].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[1][2].getOwner() == player) target = i;
+					break;
+				case 4:
+					if (b.squares[0][0].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					if (b.squares[2][0].getOwner() == player && b.squares[0][2].getOwner() == player) target = i;
+					if (b.squares[0][1].getOwner() == player && b.squares[2][1].getOwner() == player) target = i;
+					if (b.squares[1][0].getOwner() == player && b.squares[1][2].getOwner() == player) target = i;
+					break;
+				case 5:
+					if (b.squares[0][2].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[1][0].getOwner() == player) target = i;
+					break;
+				case 6:
+					if (b.squares[0][0].getOwner() == player && b.squares[1][0].getOwner() == player) target = i;
+					if (b.squares[2][1].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					if (b.squares[1][1].getOwner() == player && b.squares[0][2].getOwner() == player) target = i;
+					break;
+				case 7:
+					if (b.squares[0][1].getOwner() == player && b.squares[1][1].getOwner() == player) target = i;
+					if (b.squares[2][0].getOwner() == player && b.squares[2][2].getOwner() == player) target = i;
+					break;
+				case 8:
+					if (b.squares[1][1].getOwner() == player && b.squares[0][0].getOwner() == player) target = i;
+					if (b.squares[1][2].getOwner() == player && b.squares[0][2].getOwner() == player) target = i;
+					if (b.squares[2][1].getOwner() == player && b.squares[2][0].getOwner() == player) target = i;
+					break;
+				default:
+					break;
+			}
+		}
+		return target;
+	}
+	
+	/// END AI, HELPERS ///
 	
 	public static int randInt(int min, int max){
 		Random rand = new Random();
